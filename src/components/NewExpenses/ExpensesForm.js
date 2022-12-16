b5f6c28fa1d88a987e51aecfd2bb3fb382fd25db
Artur Khliab <black11051999@gmail.com>
2022-12-16 16:57:28 +0200

    Update submit form

--- src/components/NewExpenses/ExpensesForm.js
+++ src/components/NewExpenses/ExpensesForm.js
@@ -25,24 +25,25 @@ const ExpensesForm = () => {
         }
 
         console.log(expenseData);
+        setEnterTitle('');
+        setEnterAmount('');
+        setEnterDate('');
     }
-
-
 
   return (
       <form onSubmit={submitHandler}>
       <div className={'new-expense__controls'}>
         <div className={'new-expense__control'}>
             <label>Title</label>
-            <input type={"text"} onChange={titleChangeHandler} />
+            <input type={"text"} value={enterTitle} onChange={titleChangeHandler} />
         </div>
         <div className={'new-expense__control'}>
             <label>Amount</label>
-            <input type={"number"} min={'0.01'} step={'0.01'} onChange={amountChangeHandler} />
+            <input type={"number"} min={'0.01'} step={'0.01'} value={enterAmount} onChange={amountChangeHandler} />
         </div>
         <div className={'new-expense__control'}>
             <label>Date</label>
-            <input type={"date"} min={'2019-12-31'} max={'2022-12-31'} onChange={dateChangeHandler} />
+            <input type={"date"} min={'2019-12-31'} max={'2022-12-31'} value={enterDate} onChange={dateChangeHandler} />
         </div>
         <div className={'new-expense__actions'}>
             <button type={"submit"}>Add Expenses</button>
