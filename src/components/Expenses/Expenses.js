d090ac3433fbe7d6e0b3acac9256132af259311a
Artur Khliab <black11051999@gmail.com>
2022-12-19 16:58:21 +0200

    New logic expenses element output

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,25 +13,14 @@ function Expenses(props) {
     }
 
     return (
-
         <Card className={'expenses'}>
             <ExpensesFilter selected={filterYear} onChangeYear={changeYearHandler}/>
-            <ExpenseItem date={props.items[0].date}
-                         title={props.items[0].title}
-                         amount={props.items[0].amount}
-            ></ExpenseItem>
-            <ExpenseItem date={props.items[1].date}
-                         title={props.items[1].title}
-                         amount={props.items[1].amount}
-            ></ExpenseItem>
-            <ExpenseItem date={props.items[2].date}
-                         title={props.items[2].title}
-                         amount={props.items[2].amount}
-            ></ExpenseItem>
-            <ExpenseItem date={props.items[3].date}
-                         title={props.items[3].title}
-                         amount={props.items[3].amount}
-            ></ExpenseItem>
+            {props.items.map((element) => (
+                <ExpenseItem
+                title={element.title}
+                amount={element.amount}
+                date={element.date}
+           /> ))}
         </Card>
     );
 }
