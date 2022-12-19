7d82fa05bf6b7a6799461b078dc2973234db1fe4
Artur Khliab <black11051999@gmail.com>
2022-12-19 18:06:39 +0200

    Add filter logic

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,20 +7,29 @@ import {useState} from "react";
 function Expenses(props) {
 
     const [filterYear, setFilter] = useState('2022')
-    const changeYearHandler = (year) => {
-        setFilter(year)
-        console.log(filterYear)
+    const changeYearHandler = (year) => {setFilter(year) }
+
+    const filterExpenses = props.items.filter((element) =>
+        element.date.getFullYear() === parseInt(filterYear)
+    );
+
+    let expensesContent = <p>No expenses found!</p>;
+
+    if (filterExpenses.length > 0) {
+        expensesContent = filterExpenses
+            .map((element) => (
+                <ExpenseItem
+                    key={Math.random().toString()}
+                    title={element.title}
+                    amount={element.amount}
+                    date={element.date}
+                />))
     }
 
     return (
         <Card className={'expenses'}>
             <ExpensesFilter selected={filterYear} onChangeYear={changeYearHandler}/>
-            {props.items.map((element) => (
-                <ExpenseItem
-                title={element.title}
-                amount={element.amount}
-                date={element.date}
-           /> ))}
+            {expensesContent}
         </Card>
     );
 }
